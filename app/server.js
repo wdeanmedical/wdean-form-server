7045395c1e8978e90296ae220bba6bfe86836cbf
Nick Sophinos <nick@wdeanmedical.com>
2019-10-23 00:54:25 -0400

    progress

--- app/server.js
+++ app/server.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-process-exit */
+// Get environment variables
+require('../config/environments')
+
+// get dependencies
+const fs = require('fs')
+
 // get or set port and protocol
-const PORT = process.env.PORT || 8080;
-const PROTOCOL = process.env.PROTOCOL || 'http';
+const PORT = process.env.PORT || 8080
+const PROTOCOL = process.env.PROTOCOL || 'http'
